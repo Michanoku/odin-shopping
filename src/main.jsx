9559c9838cc9ee37717e462640100fa5d205d817
Michanoku <michael.werker@gmail.com>
2026-04-03 01:08:32 +0000

    feat: Add initial root css

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,16 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import "./css/index.css"
+import "./css/index.css";
+import Root from "./routes/root.jsx";
+import ErrorPage from "./routes/error-page.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root/>,
+    errorElement: <ErrorPage/>,
+    children: [],
   },
 ]);
 
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,9 +1,11 @@
+import '../css/root.css';
+
 export default function Root() {
   return (
-    <>
+    <div className="rootDiv">
         {/* Navbar Component */}
         {/* Sidebar Component */}
         {/* Page Content (Outlet?) */}
-    </>
+    </div>
   );
 }
