0a397ab32f6777201074884a5a7867d93d606938
Michanoku <michael.werker@gmail.com>
2026-04-03 02:43:29 +0000

    feat: Add initial bars layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,14 @@
+import { Menu, ShoppingCart } from 'lucide-react';
+
 export default function NavBar({toggleSideBar}) {
 
     return (
         <div className="navBar">
-            <button className="sideBarToggle" type="button" onClick={() => toggleSideBar()}>SIDEBAR</button>
+            <button className="sideBarToggle svgButton" type="button" onClick={() => toggleSideBar()}><Menu size={32} /></button>
+            <h1 className="navTitle">Michanoku Shopping</h1>
+            <input className="search" type="search"/>
+            <button className="cartButton svgButton" type="button"><ShoppingCart size={32} /></button>
         </div>
+
     )
 }
